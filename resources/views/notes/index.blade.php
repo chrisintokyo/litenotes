e7cb4b43a59568b16b72cdcc51fb0b5137447d2b
CJ <chrisintokyojapan89@gmail.com>
2023-12-15 19:14:07 +0900

    Delete feature

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,20 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Notes') }}
         </h2>
     </x-slot>
-<div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          @foreach ($notes as $note)
+
+            <x-alert-success>
+                {{ session('success') }}
+            </x-alert-success>
+@if(request())
+            <a href="{{ route('notes.create') }}" class="btn-link btn-lg mb-2">+ New Note</a>
+
+            @forelse ($notes as $note)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    <h2 class="font-bold text-2xl">
+                        <a href="{{ route('notes.show', $note) }}">{{ $note->title }}</a>
+                    </h2>
+                    <p class="mt-2">
+                        {{ Str::limit($note->text, 200) }}
+                    </p>
+                    <span class="block mt-4 text-sm opacity-70">{{ $note->updated_at->diffForHumans() }}</span>
+                </div>
+            @empty
+            <p>You have no notes yet.</p>
+            @endforelse
+
+            {{ $notes->links() }}
         </div>
-        <h2>
-         <!--   {{ $note->title }}
-        </h2>
-        <p>
-            // {{ $note->text }}
-        </p>
     </div>
 </x-app-layout>
